0bd7fd60c0442b8d39ce2dd5a30eaebf63bfb64a
MOPHE <ndachimya@gmail.com>
2025-04-16 23:01:05 +0100

    Added animation...

--- lib/widgets/action_button.dart
+++ lib/widgets/action_button.dart
@@ -5,6 +5,7 @@ class ActionButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback? onPressed;
   final Color backgroundColor;
+  static double _scale = 1.0;
 
   const ActionButton({
     super.key,
@@ -16,19 +17,48 @@ class ActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor,
-        foregroundColor: Colors.white,
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-      ),
-      icon: Icon(icon, size: 30, color: Colors.white),
-      label: Text(
-        label,
-        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTapDown: (_) => _onTapDown(context),
+      onTapUp: (_) => _onTapUp(context),
+      onTapCancel: () => _onTapCancel(context),
+      child: AnimatedScale(
+        scale: _scale,
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeInOut,
+        child: ElevatedButton.icon(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: backgroundColor,
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          icon: Icon(icon, size: 30, color: Colors.white),
+          label: Text(
+            label,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
+  }
+
+  void _onTapDown(BuildContext context) {
+    _updateScale(context, 0.9);
+  }
+
+  void _onTapUp(BuildContext context) {
+    _updateScale(context, 1.0);
+  }
+
+  void _onTapCancel(BuildContext context) {
+    _updateScale(context, 1.0);
+  }
+
+  void _updateScale(BuildContext context, double newScale) {
+    (context as Element).markNeedsBuild();
+    _scale = newScale;
   }
 }
--- lib/widgets/circle_button.dart
+++ lib/widgets/circle_button.dart
@@ -3,25 +3,48 @@ import 'package:flutter/material.dart';
 class CircleButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback? onPressed;
+  static double _scale = 1.0;
 
   const CircleButton({super.key, required this.icon, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: InkWell(
-        onTap: onPressed,
-        child: Ink(
-          decoration: BoxDecoration(
-            color: onPressed != null ? Colors.blue : Colors.grey,
-            shape: BoxShape.circle,
+    return GestureDetector(
+      onTapDown: (_) => _onTapDown(context),
+      onTapUp: (_) => _onTapUp(context),
+      onTapCancel: () => _onTapCancel(context),
+      child: AnimatedScale(
+        scale: _scale,
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeInOut,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onPressed,
+            borderRadius: BorderRadius.circular(30),
+            child: Ink(
+              decoration: BoxDecoration(
+                color: onPressed != null ? Colors.blue : Colors.grey,
+                shape: BoxShape.circle,
+              ),
+              width: 40,
+              height: 40,
+              child: Icon(icon, size: 30, color: Colors.white),
+            ),
           ),
-          width: 40,
-          height: 40,
-          child: Icon(icon, size: 30, color: Colors.white),
         ),
       ),
     );
+  }
+
+  void _onTapDown(BuildContext context) => _updateScale(context, 0.9);
+
+  void _onTapUp(BuildContext context) => _updateScale(context, 1.0);
+
+  void _onTapCancel(BuildContext context) => _updateScale(context, 1.0);
+
+  void _updateScale(BuildContext context, double newScale) {
+    (context as Element).markNeedsBuild();
+    _scale = newScale;
   }
 }
